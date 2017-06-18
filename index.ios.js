adf7c4476e818e96c41b82e3d9530616c22dfb27
Taegyum Kim <taegyumk@usc.edu>
2017-06-18 16:10:55 -0700

    Image effect on the first page

--- index.ios.js
+++ index.ios.js
@@ -12,10 +12,11 @@ import {
   Slider,
   Button,
   Alert,
-  Image
+  Image,
 } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 import { StackNavigator } from 'react-navigation';
+import ImageSlider from 'react-native-image-slider';
 import styles from './class/Style';
 import SignUp from './class/SignUp'
 import RegimenInfomation from './class/RegimenInfomation'
@@ -23,25 +24,39 @@ import Condition from './class/Condition'
 
 export default class project extends Component {
   constructor(props, context) {
-    super(props, context)
-  }
+    super(props, context);
+    this.state = {
+      position: 1,
+      interval: null
+    };
+  };
   static navigationOptions = {
     title: 'Welcome',
   };
+  componentWillMount() {
+      this.setState({interval: setInterval(() => {
+          this.setState({position: this.state.position === 2 ? 0 : this.state.position + 1});
+      }, 2000)});
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.state.interval);
+  }
   render() {
       /*temp pic setting. it should be change later*/
       let pic = {
          uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
       };
       return (
-        <View style =  {{ 
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          }}>
+        <View>
           <View>
-            <Image source={pic} style={{width: 193, height: 110}}/>
+            <ImageSlider images = {[
+              require('./images/brian.jpg'),
+              require('./images/Taegyum.jpg'),
+              require('./images/Kian.jpg')]}
+              height = {400}
+              position={this.state.position}
+              onPositionChanged={position => this.setState({position})}/>
           </View>
           <Button
             onPress = {() => this.props.navigation.navigate('SignUp')}
