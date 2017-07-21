707037505229cf9b8eaf804ee39e7c78b8cea296
Brian Cohn <bcohn12@gmail.com>
2017-07-21 16:04:50 -0700

    specified based on default export. #13

--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux'
-import people from './people'
+import {peopleReducer} from './people'
 
 const rootReducer = combineReducers({
-  people
+  people : peopleReducer
 })
 
 export default rootReducer
