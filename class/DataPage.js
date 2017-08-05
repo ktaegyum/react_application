0a1264e61ec89ca77bf307460e582ab50656b76e
Taegyum Kim <taegyumk@usc.edu>
2017-08-05 12:00:56 -0700

    #19 Simple bar graph for data page

--- class/DataPage.js
+++ class/DataPage.js
@@ -13,10 +13,42 @@ import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-
 import {styles} from './Style';
 
 export default class DataPage extends Component {
-  
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+    };
+  };
   render() {
     return (
-      <Text>TODAY IS INFUSION</Text>
+      <View>
+        <View style>
+          <Text>MIN(Fatigue)</Text>
+          <View style={{flexDirection: 'row'}}>
+            <View style ={{marginRight: 5}}><Text>M :</Text></View>
+            <View style={{width: 50, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style ={{marginRight: 5}}><Text>T  :</Text></View>
+            <View style={{width: 60, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style ={{marginRight: 5}}><Text>W :</Text></View>
+            <View style={{width: 10, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style ={{marginRight: 5}}><Text>R  :</Text></View>
+            <View style={{width: 70, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style ={{marginRight: 5}}><Text>F  :</Text></View>
+            <View style={{width: 10, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+          </View>
+          <View style={{flexDirection: 'row'}}>
+            <View style ={{marginRight: 5}}><Text>S  :</Text></View>
+            <View style={{width: 100, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+          </View>
+        </View>
+      </View>
     )
   };
 }
