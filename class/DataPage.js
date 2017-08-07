c91ff926380dec13f9b07a7687c012051107a4c7
Brian Cohn <bcohn12@gmail.com>
2017-08-07 12:04:51 -0700

    working plots for fatigue, anxiety, appetite #20

--- class/DataPage.js
+++ class/DataPage.js
@@ -30,11 +30,12 @@ export default class DataPage extends Component {
     };
   };
   render() {
-    const listItems = this.props.observations.map((ob) =>{
+    const fatigueItems = this.props.observations.map((ob) =>{
       dateTimeOfObservation = new Date(Date.parse(ob.entry_datetime))
       dayIndexOfWeek = dateTimeOfObservation.getDay()
       dayOfMonth = dateTimeOfObservation.getDate()
       monthNum = dateTimeOfObservation.getMonth()
+      observation_width = ob.fatigue
       return(
             <View style={{flexDirection: 'row'}}>
               <View style ={{marginRight: 5}}>
@@ -42,16 +43,58 @@ export default class DataPage extends Component {
                   {["M","T","W","R","F","S","U"][dayIndexOfWeek]}:
                 </Text>
               </View>
-              <View style={{width: 80, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+              <View style={{width: observation_width*10, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
             </View>
       )}
     );
+
+
+    const anxietyItems = this.props.observations.map((ob) =>{
+      dateTimeOfObservation = new Date(Date.parse(ob.entry_datetime))
+      dayIndexOfWeek = dateTimeOfObservation.getDay()
+      dayOfMonth = dateTimeOfObservation.getDate()
+      monthNum = dateTimeOfObservation.getMonth()
+      observation_width = ob.anxiety
+      return(
+            <View style={{flexDirection: 'row'}}>
+              <View style ={{marginRight: 5}}>
+                <Text>
+                  {["M","T","W","R","F","S","U"][dayIndexOfWeek]}:
+                </Text>
+              </View>
+              <View style={{width: observation_width*10, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+            </View>
+      )}
+    );
+
+    const appetiteItems = this.props.observations.map((ob) =>{
+      dateTimeOfObservation = new Date(Date.parse(ob.entry_datetime))
+      dayIndexOfWeek = dateTimeOfObservation.getDay()
+      dayOfMonth = dateTimeOfObservation.getDate()
+      monthNum = dateTimeOfObservation.getMonth()
+      observation_width = ob.lack_of_appetite
+      return(
+            <View style={{flexDirection: 'row'}}>
+              <View style ={{marginRight: 5}}>
+                <Text>
+                  {["M","T","W","R","F","S","U"][dayIndexOfWeek]}:
+                </Text>
+              </View>
+              <View style={{width: observation_width*10, height: 10, backgroundColor: 'blue', marginTop: 4 }} />
+            </View>
+      )}
+    );
+
     return (
       <ScrollView>
         <Text>Fatigue</Text>
 
-        <View>
-          {listItems}
+        <View style={{marginLeft:50}}>
+          {fatigueItems}
+          <Text>Anxiety</Text>
+          {anxietyItems}
+          <Text>Lack of Appetite</Text>
+          {appetiteItems}
           <Text>{JSON.stringify(this.props.observations)}</Text>
         </View>
       </ScrollView>
