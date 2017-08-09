8b20fd5fcd64cff26241cec367cb428f0178ed39
Brian Cohn <bcohn12@gmail.com>
2017-08-09 16:35:10 -0700

    merge master into visualupdate

--- reducers/people.js
+++ reducers/people.js
@@ -44,350 +44,205 @@ const initialState = {
       "fatigue": 1,
       "anxiety": 2,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-02T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*29,
->>>>>>> master
       "nausea": 2,
       "fatigue": 4,
       "anxiety": 2,
       "lack_of_appetite": 4
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-03T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*28,
->>>>>>> master
       "nausea": 3,
       "fatigue": 3,
       "anxiety": 1,
       "lack_of_appetite": 3
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-04T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*27,
->>>>>>> master
       "nausea": 3,
       "fatigue": 3,
       "anxiety": 1,
       "lack_of_appetite": 3
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-05T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*26,
->>>>>>> master
       "nausea": 3,
       "fatigue": 2,
       "anxiety": 0,
       "lack_of_appetite": 2
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-06T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*25,
->>>>>>> master
       "nausea": 2,
       "fatigue": 2,
       "anxiety": 0,
       "lack_of_appetite": 2
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-07T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*24,
->>>>>>> master
       "nausea": 1,
       "fatigue": 2,
       "anxiety": 0,
       "lack_of_appetite": 2
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-08T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*23,
->>>>>>> master
       "nausea": 1,
       "fatigue": 2,
       "anxiety": 0,
       "lack_of_appetite": 2
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-09T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*22,
->>>>>>> master
       "nausea": 1,
       "fatigue": 2,
       "anxiety": 0,
       "lack_of_appetite": 2
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-10T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*21,
->>>>>>> master
       "nausea": 0,
       "fatigue": 1,
       "anxiety": 0,
       "lack_of_appetite": 1
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-11T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*20,
->>>>>>> master
       "nausea": 0,
       "fatigue": 1,
       "anxiety": 0,
       "lack_of_appetite": 1
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-12T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*19,
->>>>>>> master
       "nausea": 0,
       "fatigue": 1,
       "anxiety": 1,
       "lack_of_appetite": 1
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-13T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*18,
->>>>>>> master
       "nausea": 0,
       "fatigue": 0,
       "anxiety": 1,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-14T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*17,
->>>>>>> master
       "nausea": 0,
       "fatigue": 0,
       "anxiety": 2,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-15T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*16,
->>>>>>> master
       "nausea": 0,
       "fatigue": 0,
       "anxiety": 3,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-16T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*15,
->>>>>>> master
       "nausea": 1,
       "fatigue": 4,
       "anxiety": 4,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-16T20:27:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*14,
->>>>>>> master
       "nausea": 3,
       "fatigue": 4,
       "anxiety": 2,
       "lack_of_appetite": 4
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-17T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*13,
->>>>>>> master
       "nausea": 3,
       "fatigue": 4,
       "anxiety": 2,
       "lack_of_appetite": 4
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-18T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*12,
->>>>>>> master
       "nausea": 4,
       "fatigue": 4,
       "anxiety": 1,
       "lack_of_appetite": 4
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-19T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*11,
->>>>>>> master
       "nausea": 4,
       "fatigue": 3,
       "anxiety": 0,
       "lack_of_appetite": 3
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-20T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*10,
->>>>>>> master
       "nausea": 3,
       "fatigue": 3,
       "anxiety": 0,
       "lack_of_appetite": 3
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-21T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*9,
->>>>>>> master
       "nausea": 2,
       "fatigue": 3,
       "anxiety": 0,
       "lack_of_appetite": 2
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-22T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*8,
->>>>>>> master
       "nausea": 2,
       "fatigue": 2,
       "anxiety": 0,
       "lack_of_appetite": 2
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-23T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*7,
->>>>>>> master
       "nausea": 1,
       "fatigue": 2,
       "anxiety": 0,
       "lack_of_appetite": 1
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-24T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*6,
->>>>>>> master
       "nausea": 0,
       "fatigue": 1,
       "anxiety": 0,
       "lack_of_appetite": 1
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-25T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*5,
->>>>>>> master
       "nausea": 0,
       "fatigue": 1,
       "anxiety": 0,
       "lack_of_appetite": 1
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-26T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*4,
->>>>>>> master
       "nausea": 0,
       "fatigue": 0,
       "anxiety": 1,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-27T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*3,
->>>>>>> master
       "nausea": 0,
       "fatigue": 0,
       "anxiety": 1,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-28T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*2,
->>>>>>> master
       "nausea": 0,
       "fatigue": 0,
       "anxiety": 2,
       "lack_of_appetite": 0
-<<<<<<< HEAD
-    }, {
-      "entry_datetime": "2017-05-29T18:25:43.511Z",
-=======
     },
     {
       "entry_datetime": Date.now() - 60*60*24*1000*1,
->>>>>>> master
       "nausea": 0,
       "fatigue": 0,
       "anxiety": 2,
@@ -530,10 +385,6 @@ export const peopleReducer = (state = initialState, action) => {
         sideEffect_pain_isClicked: true
       }
     case ADD_SYMPTOMS:
-<<<<<<< HEAD
-      state.symptom_observations.push({"entry_datetime": action.content.date, "nausea": action.content.fatigue, "fatigue": action.content.nausea, "anxiety": action.content.fever, "lack_of_appetite": action.content.pain})return {
-        ...state
-=======
         state.symptom_observations.push(
         {
           "entry_datetime": action.content.date,
@@ -544,7 +395,6 @@ export const peopleReducer = (state = initialState, action) => {
         })
       return {
         ...state,
->>>>>>> master
       }
     default:
       return state
